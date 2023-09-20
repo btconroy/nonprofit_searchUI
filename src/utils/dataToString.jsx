fb5b7b5eff2b94389b293b7f8eaeeea2af37460a
btconroy <loc.webdeveloper@gmail.com>
2023-09-20 09:22:41 -0400

    function to create uri string

---
+++ src/utils/dataToString.jsx
@@ -0,0 +1,34 @@
+function spaceConvert(str) {
+    let regex = /\s/g;
+    str.replace(regex, "%20");
+}
+
+
+
+
+export function dataToString(obj) {
+    const queryStr = spaceConvert(obj.query);
+    const stateStr = obj.state;
+    const nteeNum = obj.ntee;
+    const taxCodeNum = obj.taxCode;
+    let uri;
+    const uriArr = ['https://projects.propublica.org/nonprofits/api/v2/search.json', `q=${queryStr}`];
+
+    if(stateStr != null) {
+        uriArr.push(`&state%5Bid%5D=${stateStr}`);
+    }
+
+    if(nteeNum != null) {
+        uriArr.push(`&ntee%5Bid%5D=${nteeNum}`);
+    }
+
+    if(taxCodeNum != null) {
+        uriArr.push(`&c_code%5Bid%5D=${taxCodeNum}`);
+    }
+
+    uri = uriArr.join('');
+
+    return uri
+
+
+}
