97e354c9900d9d8d2a53707e2a60c55b4dc93a26
btconroy <loc.webdeveloper@gmail.com>
2023-09-28 10:12:47 -0400

    pagination arrows scroll functionality

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './Pagination.css'
 
 export default function Pagination(props) {
@@ -7,10 +7,18 @@ export default function Pagination(props) {
     const current = typeof props.current != 'number' ? Number(props.current) : props.current;
 
     const [pageLength, setPagelength] = useState(length);
-    const [currentPage, setCurrentPage] = useState(current)
+    const [currentPage, setCurrentPage] = useState(current);
+    const [pagePosition, setPagePosition] = useState({ transform: 'translateX(0px)' });
+    const pageListScroller = useRef(null);
+
+    function scrollOver(currentNum) {
+      let pageItemWidth = pageListScroller.current.children[0].offsetWidth;
+        setPagePosition({ transform: `translateX(-${pageItemWidth * currentNum}px)` });
+    }
 
     function addNumbers(len, cur) {
-        const pagesArr = []
+        const pagesArr = [];
+        
         for(let i = 0; i < len; i++) {
         let num = i + 1;
             if(i === cur) {
@@ -19,20 +27,30 @@ export default function Pagination(props) {
                 pagesArr.push(<span className='pg-num' data-pg={num} key={num}>{num}</span>);
             }
         }
-     
         return pagesArr;
+    }
+
+    function pageSelectHandler(bool) {
+        return () => {
+            if(bool) {
+                setCurrentPage(currentPage + 1 > pageLength - 1 ? pageLength - 1 : currentPage + 1)
+            } else {
+                setCurrentPage(currentPage - 1 < 0 ? 0 : currentPage - 1);
+            }
+            scrollOver(currentPage)
+        } 
     }
 
     return (
         <div id="pagination__container"> 
             <div id="pagination__inner-container">
-                <span className="arrow" role="button">&#9660;</span>
+                <span onClick={ pageSelectHandler(false) } className="arrow" role="button">&#9660;</span>
                 <div id="pagination__number-container">
-                    <div id="pagination__inner-number-container">
-                        { pageLength > 0 ? addNumbers(pageLength, currentPage) : <span className='pg-num-current'>0</span> }
+                    <div ref={pageListScroller} id="pagination__inner-number-container" style={ pagePosition }>
+                        { pageLength > 0 ? addNumbers(pageLength, currentPage) : <span className='pg-num-current'>1</span> }
                     </div>
                 </div>
-                <span className="arrow" role='button'>&#9650;</span>
+                <span onClick={ pageSelectHandler(true) } className="arrow" role='button'>&#9650;</span>
             </div>
         </div>
     )
